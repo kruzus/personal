6ec68337f7365c3073d70f935c8b8efc8c4fdaa2
Bek <kruzus@live.com>
2021-05-21 19:07:06 -0400

    fixed router

--- src/App/Layout.jsx
+++ src/App/Layout.jsx
@@ -1,9 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { About } from "../views/About";
+import { Projects } from "../views/Projects";
+import { Home } from "../views/Home";
 import { Navbar } from "../components/Navbar";
 
 export const Layout = () => {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
+function NoMatch() {
+  const location = useLocation();
+  return (
+    <>
+      No match for <code>{location.pathname}</code>
+    </>
+  );
+}
